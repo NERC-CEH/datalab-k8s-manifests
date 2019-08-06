6faac9819316ba315b8a3991197e2de5a72323a8
Alex Lee <alex.lee@tessella.com>
2019-08-06 12:29:02 +0100

    chore: add mongo user name and password config

--- config/mongo/import.sh
+++ config/mongo/import.sh
@@ -7,10 +7,12 @@ echo "Job Import started: $(date)"
 MONGO_HOST=${MONGO_HOST:-localhost}
 MONGO_PORT=${MONGO_PORT:-27017}
 MONGO_DATALAB_DB_NAME=${MONGO_DATALAB_DB_NAME:-datalab}
+MONGO_USER=${MONGO_USER:-datalabs-root-u}
+MONGO_PASSWORD=${MONGO_PASSWORD:-datalabs-root-p}
 
-echo "Vairables set: $(date)"
+echo "Variables set: $(date)"
 
-mongoimport --host $MONGO_HOST:$MONGO_PORT --db $MONGO_DATALAB_DB_NAME --collection datalabs --type json --drop --file /seeddata/datalabsCollection.json --jsonArray
-mongoimport --host $MONGO_HOST:$MONGO_PORT --db $MONGO_DATALAB_DB_NAME --collection dataStorage --type json --drop --file /seeddata/dataStorageCollection.json --jsonArray
+mongoimport -u $MONGO_USER -p $MONGO_PASSWORD --host $MONGO_HOST:$MONGO_PORT --db $MONGO_DATALAB_DB_NAME --collection datalabs --type json --drop --file /seeddata/datalabsCollection.json --jsonArray --authenticationDatabase admin
+mongoimport -u $MONGO_USER -p $MONGO_PASSWORD --host $MONGO_HOST:$MONGO_PORT --db $MONGO_DATALAB_DB_NAME --collection dataStorage --type json --drop --file /seeddata/dataStorageCollection.json --jsonArray --authenticationDatabase admin
 
 echo "Job Import finished: $(date)"
