a187a7fb1f3115ecf1c1af17848e8c655b2fe0bd
Gary Lloyd <gareth.lloyd@tessella.com>
2019-09-04 16:33:48 +0100

    feat: deploy 0.24.0-20-g07ba91b

--- config/mongo/import.sh
+++ config/mongo/import.sh
@@ -7,6 +7,7 @@ echo "Job Import started: $(date)"
 MONGO_HOST=${MONGO_HOST:-localhost}
 MONGO_PORT=${MONGO_PORT:-27017}
 MONGO_DATALAB_DB_NAME=${MONGO_DATALAB_DB_NAME:-datalab}
+MONGO_USERS_DB_NAME=${MONGO_USERS_DB_NAME:-users}
 MONGO_USER=${MONGO_USER:-datalabs-root-u}
 MONGO_PASSWORD=${MONGO_PASSWORD:-datalabs-root-p}
 
@@ -14,5 +15,6 @@ echo "Variables set: $(date)"
 
 mongoimport -u $MONGO_USER -p $MONGO_PASSWORD --host $MONGO_HOST:$MONGO_PORT --db $MONGO_DATALAB_DB_NAME --collection datalabs --type json --drop --file /seeddata/datalabsCollection.json --jsonArray --authenticationDatabase admin
 mongoimport -u $MONGO_USER -p $MONGO_PASSWORD --host $MONGO_HOST:$MONGO_PORT --db $MONGO_DATALAB_DB_NAME --collection dataStorage --type json --drop --file /seeddata/dataStorageCollection.json --jsonArray --authenticationDatabase admin
+mongoimport -u $MONGO_USER -p $MONGO_PASSWORD --host $MONGO_HOST:$MONGO_PORT --db $MONGO_USERS_DB_NAME --collection userRoles --type json --drop --file /seeddata/userRoles.json --jsonArray --authenticationDatabase admin
 
 echo "Job Import finished: $(date)"
